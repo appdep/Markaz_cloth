2600f02723abfdd7504d6e22406a5f460c361797
appdep <appdeployment64@gmail.com>
2024-04-17 00:50:25 -0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { client } from '../lib/client';
-import { HeroBanner, EventsBanner, Newsletter, FeaturesBanner, Product } from '../components';
+import React from 'react'
+import {client} from '../lib/client'
+import { HeroBanner, EventsBanner, Newsletter, FeaturesBanner, Product } from '../components'
+import { Navigation, A11y, Lazy } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css'; // Import Swiper styles
-import 'swiper/css/navigation'; // Import Swiper navigation styles
-import 'swiper/css/lazy'; // Import Swiper lazy loading styles
-import { Navigation, A11y } from 'swiper';
 
-// Import Swiper custom styles
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
 
-
-const Home = ({ products, bannerData, event1Data, event2Data, event3Data }) => {
+const Home = ({products ,bannerData ,event1Data,event2Data,event3Data}) => {
   return (
     <>
+      
+     
+
       <div className='products-outer-container'>
         <div className='subtitle'>
           <span>PRODUCTS</span>
@@ -20,68 +21,65 @@ const Home = ({ products, bannerData, event1Data, event2Data, event3Data }) => {
         </div>
         <Swiper
           breakpoints={{
-            // width >= 300
-            300: {
-              slidesPerView: 1,
-              spaceBetween: 100,
-            },
-            // width >= 1000
-            1000: {
-              slidesPerView: 2,
-              spaceBetween: 0,
-            },
-            // width >= 1260
-            1260: {
-              slidesPerView: 3,
-              spaceBetween: 0,
-            },
+              // width >= 300
+              300: {
+                slidesPerView: 1,
+                spaceBetween: 100
+              },
+              // width >= 1000
+              1000: {
+                slidesPerView: 2,
+                spaceBetween: 0
+              },
+              // width >= 1260
+              1260: {
+                slidesPerView: 3,
+                spaceBetween: 0
+              }
           }}
           modules={[Navigation, A11y]}
           spaceBetween={0}
           slidesPerView={3}
-          navigation
+          navigation={true}
         >
           <div className='products-container'>
-            {products?.map((product) => (
-              <SwiperSlide key={product._id}>
-                <Product product={product}  />
+            {products?.map(product => (
+              <SwiperSlide>
+                <Product key={product._id} product={product} />
               </SwiperSlide>
             ))}
           </div>
         </Swiper>
       </div>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-      <EventsBanner
-        event1={event1Data.length && event1Data[0]}
-        event2={event2Data.length && event2Data[0]}
-        event3={event3Data.length && event3Data[0]}
-      />
+      <EventsBanner event1={event1Data.length && event1Data[0]} event2={event2Data.length && event2Data[0]} event3={event3Data.length && event3Data[0]}/>
       <FeaturesBanner />
       <Newsletter />
     </>
-  );
-};
+  )
+}
 
 export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
   const products = await client.fetch(query);
   const bannerQuery = '*[_type == "banner"]';
-  const bannerData = await client.fetch(bannerQuery);
+   const bannerData = await client.fetch(bannerQuery);
 
-  //events
+   //events
 
-  const event1Query = '*[_type == "event1"]';
-  const event1Data = await client.fetch(event1Query);
+   const event1Query = '*[_type == "event1"]';
+   const event1Data = await client.fetch(event1Query);
 
-  const event2Query = '*[_type == "event2"]';
-  const event2Data = await client.fetch(event2Query);
+   const event2Query = '*[_type == "event2"]';
+   const event2Data = await client.fetch(event2Query);
 
-  const event3Query = '*[_type == "event3"]';
-  const event3Data = await client.fetch(event3Query);
+   const event3Query = '*[_type == "event3"]';
+   const event3Data = await client.fetch(event3Query);
 
+   
   return {
-    props: { products, bannerData, event1Data, event2Data, event3Data },
-  };
-};
+    props: { products, bannerData , event1Data,event2Data,event3Data}
+  }
+}
 
-export default Home;
+export default Home
